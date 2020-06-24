c7b3e9df05fa2c1c9369a7c6813076caa6db0baf
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-06-25 07:40:45 +0900

    パス修正

--- src/main/java/com/example/struts2/example/actions/package-info.java
+++ src/main/java/com/example/struts2/example/actions/package-info.java
@@ -1,2 +1,3 @@
-@org.apache.struts2.convention.annotation.Namespace("application")
+@org.apache.struts2.convention.annotation.Namespace("")
+@org.apache.struts2.convention.annotation.ParentPackage("com.example.struts2.example.actions")
 package com.example.struts2.example.actions;
